de16c4c4f121260d6dcb48191b1a6c0e044f22a1
bahrijar <muhammadbahrijar4@gmail.com>
2022-07-26 10:13:01 +0700

    4 files modified, 1 added

--- 04-wine/controllers/WineController.js
+++ 04-wine/controllers/WineController.js
@@ -1,6 +1,17 @@
+const Wine = require('../models/Wine');
+const WineView = require('../views/WineView');
+
 class WineController {
-  static show() {}
+  static help() {
+    WineView.help();
+  }
+  static findAll() {
+    Wine.findAll();
+  }
+  static findById() {}
   static create(params) {}
   static update(params) {}
   static delete(params) {}
 }
+
+module.exports = WineController;
--- 04-wine/models/Wine.js
+++ 04-wine/models/Wine.js
@@ -1,3 +1,5 @@
+const fs = require('fs');
+
 class Wine {
   constructor(id, name, year, type, createdAt) {
     this.id = id;
@@ -6,4 +8,20 @@ class Wine {
     this.type = type;
     this.createdAt = createdAt;
   }
+
+  static findAll() {
+    const wines = JSON.parse(fs.readFileSync('./data.json'));
+    console.log(wines);
+  }
+  static findById() {}
+  static create() {}
+  static update() {}
+  static delete() {}
+  static sell() {}
+
+  static save(wines) {
+    fs.writeFileSync('./data.json', JSON.stringify(wines, null, 3));
+  }
 }
+
+module.exports = Wine;
