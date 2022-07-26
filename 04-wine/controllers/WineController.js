a94c827232554f3c89a204a6c99980d80bdf4cef
bahrijar <muhammadbahrijar4@gmail.com>
2022-07-26 09:43:39 +0700

    5 files added

---
+++ 04-wine/controllers/WineController.js
@@ -0,0 +1,6 @@
+class WineController {
+  static show() {}
+  static create(params) {}
+  static update(params) {}
+  static delete(params) {}
+}
---
+++ 04-wine/models/Wine.js
@@ -0,0 +1,9 @@
+class Wine {
+  constructor(id, name, year, type, createdAt) {
+    this.id = id;
+    this.name = name;
+    this.year = year;
+    this.type = type;
+    this.createdAt = createdAt;
+  }
+}
